c0fcf547c634addd0f92ce2a55d244ba920892bc
Nishant Madan <nishant.madan@xebia.com>
2019-08-14 13:03:23 +0530

    Files added of main project

---
+++ src/main/java/com/xebia/okrapplication/okrapplication/OkrapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.okrapplication.okrapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrapplicationApplication.class, args);
+	}
+
+}
