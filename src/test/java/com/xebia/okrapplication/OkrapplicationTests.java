0c9e404f831bd8bf191f6016b6dcef99e4455ce1
Nishant Madan <nishant.madan@xebia.com>
2019-09-04 16:59:56 +0530

    Update the test for all repository

--- src/test/java/com/xebia/okrapplication/OkrapplicationTests.java
+++ src/test/java/com/xebia/okrapplication/OkrapplicationTests.java
@@ -1,10 +1,12 @@
 package com.xebia.okrapplication;
 
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OkrapplicationTests {
 
+	@Test
 	public void contextLoads() {
 
 	}
